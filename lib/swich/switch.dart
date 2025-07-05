d39812089612ee253c60d9e8168f53bcbd182797
Mynul Alam <mynulalam27@gmail.com>
2025-07-05 20:19:23 +0600

    switch

---
+++ lib/swich/switch.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SwitchPage extends StatelessWidget {
+  const SwitchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
