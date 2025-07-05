371a50adeb0da5e47febbd65ec1b2fb4ff7fe224
Mynul Alam <mynulalam27@gmail.com>
2025-07-05 19:46:26 +0600

    liquid swip

---
+++ lib/Liquid_swip/liquid_swip.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LiquidSwip extends StatelessWidget {
+  const LiquidSwip({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Container()));
+  }
+}
