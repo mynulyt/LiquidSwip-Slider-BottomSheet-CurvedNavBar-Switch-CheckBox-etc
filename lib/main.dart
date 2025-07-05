c2763f306200c2426378257c6d7f72b86c1b14a7
Mynul Alam <mynulalam27@gmail.com>
2025-07-05 19:55:55 +0600

    commit main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget/Liquid_swip/liquid_swip.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(title: const Text('Material App Bar')),
-        body: const Center(child: Text('Hello World')),
+        body: LiquidSwip(),
       ),
     );
   }
