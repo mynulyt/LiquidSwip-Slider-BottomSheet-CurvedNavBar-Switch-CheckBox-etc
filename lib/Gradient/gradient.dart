7bb4994e8e16a3a652186f050ed9cd5b492924d9
Mynul Alam <mynulalam27@gmail.com>
2025-07-05 20:01:46 +0600

    gradient

--- lib/Gradient/gradient.dart
+++ lib/Gradient/gradient.dart
@@ -5,6 +5,11 @@ class GradientPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+      ),
+    );
   }
 }
