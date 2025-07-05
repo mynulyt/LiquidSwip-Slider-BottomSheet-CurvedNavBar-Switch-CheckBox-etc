1feea3427b7c14b4e244ec44c6ddcf066607a939
Mynul Alam <mynulalam27@gmail.com>
2025-07-05 20:26:10 +0600

    switch

--- lib/swich/switch.dart
+++ lib/swich/switch.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
-class SwitchPage extends StatelessWidget {
+class SwitchPage extends StatefulWidget {
   const SwitchPage({super.key});
 
   @override
+  State<SwitchPage> createState() => _SwitchPageState();
+}
+
+class _SwitchPageState extends State<SwitchPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    bool _value = false;
+    return Scaffold(
+      body: Center(
+        child: Switch(value: _value, onChanged: (val){
+          setState((){
+
+_value = val;
+print("Work")
+          });
+        
+        }),
+      ),
+    );
   }
 }
