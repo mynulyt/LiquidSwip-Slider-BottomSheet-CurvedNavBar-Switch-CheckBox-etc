b7668f4ca715e75e728d9cc0663ce93a330caac3
Mynul Alam <mynulalam27@gmail.com>
2025-07-06 00:23:20 +0600

    tab bar done

--- lib/Tab_bar/tab_bar.dart
+++ lib/Tab_bar/tab_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget/Tab_bar/page1.dart';
+import 'package:flutter_widget/Tab_bar/page2.dart';
 
 class TabBarPage extends StatelessWidget {
   const TabBarPage({super.key});
@@ -10,9 +12,10 @@ class TabBarPage extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           bottom: TabBar(
-            tabs: [Tab(icon: Icon(Icons.call)), Tab(icon: Icon(Icons.call))],
+            tabs: [Tab(icon: Icon(Icons.call)), Tab(icon: Icon(Icons.message))],
           ),
         ),
+        body: TabBarView(children: [Page1(), Page2()]),
       ),
     );
   }
