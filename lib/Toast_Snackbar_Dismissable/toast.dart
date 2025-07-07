c924ac250d3c314a70921f46f5fa249d64416282
Mynul Alam <mynulalam27@gmail.com>
2025-07-07 21:25:37 +0600

    Toast

---
+++ lib/Toast_Snackbar_Dismissable/toast.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ToastMessage extends StatelessWidget {
+  const ToastMessage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
