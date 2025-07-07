51d39546e59bb5aedc05496a026ac0618191f3eb
Mynul Alam <mynulalam27@gmail.com>
2025-07-08 01:42:41 +0600

    dismissable

--- lib/Toast_Snackbar_Dismissable/dismissable.dart
+++ lib/Toast_Snackbar_Dismissable/dismissable.dart
@@ -7,10 +7,18 @@ class DismissablePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ListTile(
-          title: Text("This is dismissable"),
-          subtitle: Text("Is this work right?"),
-          trailing: Icon(Icons.delete),
+        child: Dismissible(
+          background: Container(
+            color: Colors.blue,
+            child: Icon(Icons.delete, size: 35),
+          ),
+
+          key: ValueKey("abc"),
+          child: ListTile(
+            title: Text("This is dismissable"),
+            subtitle: Text("Is this work right?"),
+            trailing: Icon(Icons.abc),
+          ),
         ),
       ),
     );
