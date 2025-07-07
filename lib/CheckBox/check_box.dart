faf909ed4ed8bacb9993d848f686e13ffe6fbdc2
Mynul Alam <mynulalam27@gmail.com>
2025-07-08 01:57:16 +0600

    checkBox

--- lib/CheckBox/check_box.dart
+++ lib/CheckBox/check_box.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 
-class CheckBoxPage extends StatelessWidget {
+class CheckBoxPage extends StatefulWidget {
   const CheckBoxPage({super.key});
 
   @override
+  State<CheckBoxPage> createState() => _CheckBoxPageState();
+}
+
+class _CheckBoxPageState extends State<CheckBoxPage> {
+  bool _value = false;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Checkbox(
+            value: _value,
+            onChanged: (bool? value) {
+              setState(() {
+                _value = value!;
+              });
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
