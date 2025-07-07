9570ec4beadf20d031558f65eedba61d11d344ee
Mynul Alam <mynulalam27@gmail.com>
2025-07-08 01:49:00 +0600

    dismissable

--- lib/Toast_Snackbar_Dismissable/dismissable.dart
+++ lib/Toast_Snackbar_Dismissable/dismissable.dart
@@ -8,6 +8,10 @@ class DismissablePage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Dismissible(
+          secondaryBackground: Container(
+            color: Colors.red,
+            child: Icon(Icons.close, size: 35),
+          ),
           background: Container(
             color: Colors.blue,
             child: Icon(Icons.delete, size: 35),
