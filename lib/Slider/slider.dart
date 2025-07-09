18f8685f252caf22d11b14e670cd949baf31007e
Mynul Alam <mynulalam27@gmail.com>
2025-07-09 22:29:34 +0600

    slider page

--- lib/Slider/slider.dart
+++ lib/Slider/slider.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
-class SliderPage extends StatelessWidget {
+class SliderPage extends StatefulWidget {
   const SliderPage({super.key});
 
   @override
+  State<SliderPage> createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  var _value = 10.0;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container());
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Slider(
+            min: 10,
+            max: 50,
+            value: _value,
+            onChanged: (value) {
+              setState(() {
+                _value = value;
+              });
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
