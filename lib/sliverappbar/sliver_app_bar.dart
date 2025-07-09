77c843c485b404f707b861b532b62111b65ad9bb
Mynul Alam <mynulalam27@gmail.com>
2025-07-09 23:03:56 +0600

    silver app bar

--- lib/sliverappbar/sliver_app_bar.dart
+++ lib/sliverappbar/sliver_app_bar.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class SliverAppBarPage extends StatelessWidget {
+class SliverAppBarPage extends StatefulWidget {
   const SliverAppBarPage({super.key});
 
+  @override
+  State<SliverAppBarPage> createState() => _SliverAppBarPageState();
+}
+
+class _SliverAppBarPageState extends State<SliverAppBarPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
