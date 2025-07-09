5905d710066bf32171b7e3eeed45195eb3084a86
Mynul Alam <mynulalam27@gmail.com>
2025-07-09 23:31:11 +0600

    sliver app bar

--- lib/sliverappbar/sliver_app_bar.dart
+++ lib/sliverappbar/sliver_app_bar.dart
@@ -8,29 +8,53 @@ class SliverAppBarPage extends StatefulWidget {
 }
 
 class _SliverAppBarPageState extends State<SliverAppBarPage> {
+  final List<Map<String, String>> userList = [
+    {"name": "Mynul", "description": "Flutter Developer"},
+    {"name": "Alam", "description": "UI Designer"},
+    {"name": "Rakib", "description": "Backend Expert"},
+    {"name": "Sakib", "description": "Full Stack Dev"},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(
+          const SliverAppBar(
             expandedHeight: 200,
             backgroundColor: Colors.red,
             flexibleSpace: FlexibleSpaceBar(
               title: Text("This is Sliver App Bar"),
             ),
+            pinned: true,
           ),
           SliverList(
-            delegate: SliverChildListDelegate(<Widget>[
-              ListTile(
-                title: Text("Title 1"),
-                subtitle: Text("SubTitle 1"),
-                leading: CircleAvatar(),
-              ),
-            ]),
+            delegate: SliverChildBuilderDelegate((context, index) {
+              final user = userList[index];
+              return CustomList(
+                name: user['name']!,
+                titleDes: user['description']!,
+              );
+            }, childCount: userList.length),
           ),
         ],
       ),
+    );
+  }
+}
+
+class CustomList extends StatelessWidget {
+  final String name;
+  final String titleDes;
+
+  const CustomList({super.key, required this.name, required this.titleDes});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(name),
+      subtitle: Text(titleDes),
+      leading: CircleAvatar(child: Text(name[0])),
     );
   }
 }
