aafc9a2a4c7363dbdd643d6d70633cc2afa7b187
Mynul Alam <mynulalam27@gmail.com>
2025-07-10 00:31:34 +0600

    hero page

---
+++ lib/Animation/hero.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HeroPage extends StatelessWidget {
+  const HeroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
