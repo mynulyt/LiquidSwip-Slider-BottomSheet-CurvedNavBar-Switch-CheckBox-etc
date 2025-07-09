f7486774dbf47acd421a8f50631244eacdc7cd41
Mynul Alam <mynulalam27@gmail.com>
2025-07-09 23:02:28 +0600

    sliver app bar

---
+++ lib/sliverappbar/sliver_app_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SliverAppBarPage extends StatelessWidget {
+  const SliverAppBarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
