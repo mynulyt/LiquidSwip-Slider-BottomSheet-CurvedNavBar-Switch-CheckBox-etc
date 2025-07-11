1e73d6754cd1050e4c7944df141b372ddf04a9ef
Mynul Alam <mynulalam27@gmail.com>
2025-07-12 01:02:37 +0600

    animated conatiner

--- lib/Animation/animated_container.dart
+++ lib/Animation/animated_container.dart
@@ -5,6 +5,9 @@ class AnimatedContainerPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column());
+    return Scaffold(body: Column(children: [
+        
+      ],
+    ));
   }
 }
