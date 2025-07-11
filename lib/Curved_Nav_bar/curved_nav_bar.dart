075f0b7cda3e9c09d3083a1ba238d0cf479ec55c
Mynul Alam <mynulalam27@gmail.com>
2025-07-12 01:01:05 +0600

    animated Container

--- lib/Curved_Nav_bar/curved_nav_bar.dart
+++ lib/Curved_Nav_bar/curved_nav_bar.dart
@@ -4,7 +4,6 @@ import 'package:flutter_widget/Gradient/gradient.dart';
 import 'package:flutter_widget/Liquid_swip/liquid_swip.dart';
 import 'package:flutter_widget/Tab_bar/page1.dart';
 import 'package:flutter_widget/Tab_bar/page2.dart';
-import 'package:flutter_widget/Tab_bar/tab_bar.dart';
 import 'package:flutter_widget/sliverappbar/sliver_app_bar.dart';
 
 class CurvedNavBar extends StatefulWidget {
