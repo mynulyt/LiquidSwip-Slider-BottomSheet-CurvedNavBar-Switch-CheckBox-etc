038e0c6d5c4992cc4f03cdc7b5831ecc7c8ab283
Mynul Alam <mynulalam27@gmail.com>
2025-07-13 21:09:05 +0600

    main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      title: 'Flutter Widget',
       home: Scaffold(
         appBar: AppBar(title: const Text('Material App Bar')),
         body: CurvedNavBar(),
