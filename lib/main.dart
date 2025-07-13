7eac112703e6b7615220078df76556af755a2f6c
Mynul Alam <mynulalam27@gmail.com>
2025-07-13 21:09:47 +0600

    main faile

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Widget',
       home: Scaffold(
-        appBar: AppBar(title: const Text('Material App Bar')),
+        appBar: AppBar(title: const Text('Flutter Widget App Bar')),
         body: CurvedNavBar(),
       ),
     );
