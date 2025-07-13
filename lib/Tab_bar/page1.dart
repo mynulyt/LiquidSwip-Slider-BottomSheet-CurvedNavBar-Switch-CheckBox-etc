090f00e2d428c2437fe5b5a9d4e9cb58b4a266d6
Mynul Alam <mynulalam27@gmail.com>
2025-07-13 21:11:45 +0600

    page 1

--- lib/Tab_bar/page1.dart
+++ lib/Tab_bar/page1.dart
@@ -5,6 +5,6 @@ class Page1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(color: Colors.purple));
+    return Scaffold(body: Container(color: Colors.yellow));
   }
 }
