c5078e3079510b20b652a004b3a056619edd2fdd
Mynul Alam <mynulalam27@gmail.com>
2025-07-14 23:45:45 +0600

    animated Crossfad

--- lib/Animation/animated_crossfade.dart
+++ lib/Animation/animated_crossfade.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AnimatedCrossfade extends StatelessWidget {
+class AnimatedCrossfade extends StatefulWidget {
   const AnimatedCrossfade({super.key});
 
+  @override
+  State<AnimatedCrossfade> createState() => _AnimatedCrossfadeState();
+}
+
+class _AnimatedCrossfadeState extends State<AnimatedCrossfade> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
