ef3b11c90340d3a0c92d052fe278662a4585b90f
Mynul Alam <mynulalam27@gmail.com>
2025-07-14 23:05:00 +0600

    animation

--- lib/Animation/animated_container.dart
+++ lib/Animation/animated_container.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-class AnimatedContainerPage extends StatelessWidget {
+class AnimatedContainerPage extends StatefulWidget {
   const AnimatedContainerPage({super.key});
 
   @override
+  State<AnimatedContainerPage> createState() => _AnimatedContainerPageState();
+}
+
+class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: [Container()]));
+    return Scaffold(body: );
   }
 }
