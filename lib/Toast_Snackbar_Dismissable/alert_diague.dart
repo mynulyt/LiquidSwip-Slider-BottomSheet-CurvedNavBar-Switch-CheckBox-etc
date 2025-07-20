1fbd0e3c4bcbcdae9de5e047312f0334c915d2b7
Mynul Alam <mynulalam27@gmail.com>
2025-07-20 23:04:22 +0600

    alert message

--- lib/Toast_Snackbar_Dismissable/alert_diague.dart
+++ lib/Toast_Snackbar_Dismissable/alert_diague.dart
@@ -5,6 +5,8 @@ class AlertDiague extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ElevatedButton(onPressed: () {}, child: Text("Alert Message")),
+    );
   }
 }
