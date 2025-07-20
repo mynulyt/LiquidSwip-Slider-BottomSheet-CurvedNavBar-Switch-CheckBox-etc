c063744b2df1f18473473ee529ba07e35e15df26
Mynul Alam <mynulalam27@gmail.com>
2025-07-20 22:58:49 +0600

    alert diague

---
+++ lib/Toast_Snackbar_Dismissable/alert_diague.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AlertDiague extends StatelessWidget {
+  const AlertDiague({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
