2130f5fc14fcc2b5064cc4dc9c0f56077d674813
Mynul Alam <mynulalam27@gmail.com>
2025-07-21 23:30:49 +0600

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_widget/Curved_Nav_bar/curved_nav_bar.dart';
 import 'package:flutter_widget/card_&_GridView/Card.dart';
+import 'package:flutter_widget/card_&_GridView/grid_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Widget',
       home: Scaffold(
         appBar: AppBar(title: const Text('Flutter Widget App Bar')),
-        body: CardDesign(),
+        body: GridViewDesign(),
       ),
     );
   }
