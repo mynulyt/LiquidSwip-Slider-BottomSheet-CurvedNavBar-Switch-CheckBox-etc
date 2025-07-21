6be57d3747479c4f4194645d5473b99806bd12ad
Mynul Alam <mynulalam27@gmail.com>
2025-07-21 23:30:13 +0600

    grid view

--- lib/card_&_GridView/grid_view.dart
+++ lib/card_&_GridView/grid_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget/card_&_GridView/Card.dart';
 
-class GridView extends StatelessWidget {
-  const GridView({super.key});
+class GridViewDesign extends StatelessWidget {
+  const GridViewDesign({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GridView.count(crossAxisCount: 2, children: [CardDesign()]),
+    );
   }
 }
