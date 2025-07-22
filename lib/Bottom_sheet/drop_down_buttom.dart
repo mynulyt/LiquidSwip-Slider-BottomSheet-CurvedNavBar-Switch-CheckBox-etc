22be76ee66214a50c87224c2d8bde99984e921a6
Mynul Alam <mynulalam27@gmail.com>
2025-07-22 22:25:53 +0600

    drop down

--- lib/Bottom_sheet/drop_down_buttom.dart
+++ lib/Bottom_sheet/drop_down_buttom.dart
@@ -10,6 +10,23 @@ class DropDownButtom extends StatefulWidget {
 class _DropDownButtomState extends State<DropDownButtom> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Column()));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButton(
+              items: [
+                DropdownMenuItem(
+                  value: "Mynul Alam",
+                  child: Text("Mynul Alam"),
+                ),
+              ],
+              onChanged: null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
